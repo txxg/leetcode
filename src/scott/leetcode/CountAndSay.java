bb789ac4c95b70fe3405d8f1d5393d26770d47a0
xingxiaoguang <xingxiaoguang@nhn.com>
2014-10-21 16:10:53 +0900

    Init commit

---
+++ src/scott/leetcode/CountAndSay.java
@@ -0,0 +1,17 @@
+package scott.leetcode;
+
+public class CountAndSay {
+
+    public CountAndSay() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
